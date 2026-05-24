923646c3a10102f1c548cd93209b234b208fb4d1
tomasmertian <tomasmertian@gmail.com>
2026-05-23 23:54:18 -0300

    modifique routes de materias para que valide la info con el middleware

--- routes/extras/materia.routes.js
+++ routes/extras/materia.routes.js
@@ -1,10 +1,11 @@
 const { Router } = require('express')
 const { getMateriaAll, getMateriaById, postMateria } = require('../../controllers/materia.controller')
+const { validateMateria } = require('../../middlewares/validateMateria')
 
 const rutas = Router()
 
 rutas.get('/', getMateriaAll)
-rutas.post('/', postMateria)
+rutas.post('/', validateMateria, postMateria)
 rutas.get('/:idMateria', getMateriaById)
 
 module.exports = rutas
