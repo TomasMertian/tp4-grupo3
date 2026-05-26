7d885efca646ae10c70cdefb7dcbf9397f9e50c5
maite surop <surop.maite@gmail.com>
2026-05-26 19:17:31 -0300

    guardando cambios

--- controllers/materia.controller.js
+++ controllers/materia.controller.js
@@ -8,7 +8,9 @@ const getMateriaAll = async (req, res) => {
     return res.status(200).json(materias)
   } catch (error) {
     console.log(error)
-    return res.status(500).json({ error: 'No se pudieron obtener las materias' })
+    return res
+      .status(500)
+      .json({ error: 'No se pudieron obtener las materias' })
   }
 }
 
@@ -19,10 +21,12 @@ const getMateriaById = async (req, res) => {
 
     const { idMateria } = req.params
 
-    const materia = materias.find(m => m.idMateria === idMateria)
+    const materia = materias.find((m) => m.idMateria === idMateria)
 
     if (!materia) {
-      return res.status(404).json({ msg: `No existe la materia con ID ${idMateria}` })
+      return res
+        .status(404)
+        .json({ msg: `No existe la materia con ID ${idMateria}` })
     }
 
     return res.status(200).json(materia)
@@ -32,4 +36,46 @@ const getMateriaById = async (req, res) => {
   }
 }
 
-module.exports = { getMateriaAll, getMateriaById }
+const updateMateria = async (req, res) => {
+  try {
+    const data = await fs.readFile('./data/extras/sys-materias.json', 'utf8')
+
+    const materias = JSON.parse(data)
+
+    const { idMateria } = req.params
+
+    const datosActualizados = req.body
+
+    const index = materias.findIndex((m) => m.idMateria === idMateria)
+
+    if (index === -1) {
+      return res.status(404).json({
+        msg: `No existe la materia con ID ${idMateria}`
+      })
+    }
+
+    materias[index] = {
+      ...materias[index],
+      ...datosActualizados
+    }
+
+    await fs.writeFile(
+      './data/extras/sys-materias.json',
+      JSON.stringify(materias, null, 2)
+    )
+
+    return res.status(200).json(materias[index])
+  } catch (error) {
+    console.log(error)
+
+    return res.status(500).json({
+      error: 'No se pudo actualizar la materia'
+    })
+  }
+}
+
+module.exports = {
+  getMateriaAll,
+  getMateriaById,
+  updateMateria
+}
--- routes/extras/materia.routes.js
+++ routes/extras/materia.routes.js
@@ -1,9 +1,14 @@
 const { Router } = require('express')
-const { getMateriaAll, getMateriaById } = require('../../controllers/materia.controller')
+const {
+  getMateriaAll,
+  getMateriaById,
+  updateMateria
+} = require('../../controllers/materia.controller')
 
 const rutas = Router()
 
 rutas.get('/', getMateriaAll)
 rutas.get('/:idMateria', getMateriaById)
+rutas.put('/:idMateria', updateMateria)
 
 module.exports = rutas
