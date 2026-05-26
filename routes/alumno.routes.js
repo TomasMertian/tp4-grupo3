589d566af710b22b12fc5914320389fedd785903
tomisanti05 <thomassantiago005@gmail.com>
2026-05-26 03:17:33 +0000

    agrego controlador postAlumno

--- routes/alumno.routes.js
+++ routes/alumno.routes.js
@@ -2,11 +2,14 @@ const { Router } = require('express')
 const {
   getAlumnoAll,
   getAlumnoById
+  //postAlumno
 } = require('../controllers/alumno.controller')
 
 const rutas = Router()
 
 rutas.get('/', getAlumnoAll)
 rutas.get('/:legajo', getAlumnoById)
+
+//rutas.post('/',validateAlumno, postAlumno)
 
 module.exports = rutas
