705198269914d3d9e8429ecbd2af8a0a6ac7b2e4
tomisanti05 <thomassantiago005@gmail.com>
2026-05-26 17:39:07 +0000

    error de rutas

--- routes/alumno.routes.js
+++ routes/alumno.routes.js
@@ -1,10 +1,11 @@
 const { Router } = require('express')
 const {
   getAlumnoAll,
-  getAlumnoById,
-  postAlumno
+  getAlumnoById
 } = require('../controllers/alumno.controller')
 
+const { postAlumno } = require('../controllers/postAlumno.controller')
+const { validateAlumno } = require('../middlewares/validateAlumno')
 const rutas = Router()
 
 rutas.get('/', getAlumnoAll)
